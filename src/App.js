aa108c637267091b117458fbd48fed465a69bf0b
cnyt-y <56654010+cnyt-y@users.noreply.github.com>
2023-08-13 20:05:54 +0300

    sss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React from 'react'
+import { useGlobalContext } from './context.js'
+
+// components
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
+// items
 
 function App() {
+  const { loading } = useGlobalContext()
+
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      merhaba dünya
-    </div>
-  );
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./index.css";
+import { AppProvider } from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <AppProvider>
+    <App />
+  </AppProvider>
 );
